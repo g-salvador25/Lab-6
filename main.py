ac2eb3641f926323628a79c2c1d1f7aa39565ab7
gabby <g.salvador@ufl.edu>
2023-10-25 15:57:07 -0400

    second commit

---
+++ main.py
@@ -0,0 +1,30 @@
+def main():
+    def menu():
+        print("Menu")
+        print("-" * 13)
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+
+    menu()
+    input = input("Please enter an option: ")
+
+    def encode():
+        global password
+        password = input("Please enter your password to encode: ")
+        global encoded_password
+        encoded_password = ""
+        for num in password:
+            new_num = int(num) + 3
+            encoded_password += str(new_num)
+
+
+    if input == "1":
+        encode()
+        print("Your password has been encoded and stored!")
+
+    elif input == "2":
+        print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+
+    elif input == "3":
